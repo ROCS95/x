cfd9646814da0858a1783440043f38e8b61dbd1f
unknown <you@example.comrobertchsoto@gmail.com>
2021-02-26 20:48:59 -0600

    ya trae el xml

--- Form1.cs
+++ Form1.cs
@@ -18,18 +18,18 @@ namespace MonolithConect
     {
         static string date = DateTime.Now.ToString("yyyy-MM-dd");
         private int counter;
-        private string DesURL = "http://192.168.200.240:7038/";
+        private string DesURL = "http://190.211.102.10:8787/";
         private string requestXml = "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
     "<soap:Header>" +
     "<HTNGHeader xmlns = \"http://htng.org/1.1/Header/\" >" +
-    "< EX_HOTEL >" +
-       " < login >" +
-           "< username > p1 </ username >" +
-           "< password > p1 </ password >" +
-        "</ login >" +
-    "</ EX_HOTEL >" +
-    "</ HTNGHeader >" +
-    "</ soap:Header>" +
+    "<EX_HOTEL>" +
+       " <login>" +
+           "<username> p1 </username>" +
+           "<password> p1 </password>" +
+        "</login>" +
+    "</EX_HOTEL>" +
+    "</HTNGHeader>" +
+    "</soap:Header>" +
     "<soap:Body>" +
     "<GET_EXT_INHOUSE>" +
        "<Query_Date>"+ date + "</Query_Date>" +
@@ -42,11 +42,12 @@ namespace MonolithConect
         {
             InitializeComponent();
             InitializeTimer();
+            textBox1.Text = postXMLData(DesURL, requestXml);
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            
         }
 
         public string postXMLData(string destinationUrl, string requestXml)
@@ -56,21 +57,23 @@ namespace MonolithConect
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(destinationUrl);
                 byte[] bytes;
                 bytes = System.Text.Encoding.ASCII.GetBytes(requestXml);
+                request.Timeout = -1;
                 request.ContentType = "text/xml; encoding='utf-8'";
                 request.ContentLength = bytes.Length;
                 request.Method = "POST";
                 Stream requestStream = request.GetRequestStream();
                 requestStream.Write(bytes, 0, bytes.Length);
-                requestStream.Close();
+                
                 HttpWebResponse response;
                 response = (HttpWebResponse)request.GetResponse();
+                requestStream.Close();
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
                     Stream responseStream = response.GetResponseStream();
                     string responseStr = new StreamReader(responseStream).ReadToEnd();
                     return responseStr;
                 }
-            
+                
             }
             catch (HttpRequestException e)
             {
@@ -97,22 +100,22 @@ namespace MonolithConect
 
         private void Timer1_Tick(object Sender, EventArgs e)
         {
-            if (counter >= 10)
+            if (counter == 10)
             {
                 // Exit loop code.  
                 //timer1.Enabled = false;
                 
-                counter = 0;
-                label1.Text = counter.ToString();
-                date = DateTime.Now.ToString("yyyy-MM-dd");
-                textBox1.Text = postXMLData(DesURL, requestXml);
+                //counter = 0;
+                //label1.Text = counter.ToString();
+                //date = DateTime.Now.ToString("yyyy-MM-dd");
+                //textBox1.Text = postXMLData(DesURL, requestXml);
             }
             else
             {
                 // Run your procedure here.  
                 // Increment counter.  
-                counter = counter + 1;
-                label1.Text = "Procedures Run: " + counter.ToString();
+                //counter = counter + 1;
+                //label1.Text = "Procedures Run: " + counter.ToString();
                 
             }
         }
